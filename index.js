c3e8703282db4c44398a8934765810110c5d6a43
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-07-20 16:47:21 +0300

    Master
    add auntification

--- index.js
+++ index.js
@@ -20,6 +20,50 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/auth/login', async (req, res) => {
+  try {
+    const user = await UserModel.findOne({
+      email: req.body.email
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'Пользователь не найден',
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHach);
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: 'Неверный логин или пароль',
+      });
+    }
+
+    const token = jwt.sign({
+        _id: user._id,
+      },
+      'secret123', {
+        expiresIn: '30d'
+      }
+    );
+
+    const {
+      passwordHach,
+      ...userData
+    } = user._doc
+
+    res.json({
+      ...userData,
+      token
+    });
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({
+      message: 'Не удалось авторизоваться'
+    });
+  }
+})
 
 app.post('/auth/register', registerValidation, async (req, res) => {
   try {
@@ -49,7 +93,10 @@ app.post('/auth/register', registerValidation, async (req, res) => {
       }
     );
 
-    const {passwordHach, ...userData} = user._doc
+    const {
+      passwordHach,
+      ...userData
+    } = user._doc
 
     res.json({
       ...userData,
@@ -61,7 +108,7 @@ app.post('/auth/register', registerValidation, async (req, res) => {
       message: 'Не удалось зарегистрироваться'
     });
   }
-})
+});
 
 app.listen(4444, (err) => {
   if (err) {
