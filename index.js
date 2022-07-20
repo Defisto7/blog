93c0f9852dc97474b7175280dfe9ad462a62209a
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-07-20 09:26:36 +0300

    master
    add validation

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+
+import { registerValidation } from './validations/auth.js'
 
 mongoose.connect('mongodb+srv://admin:5102004@cluster0.jshki4o.mongodb.net/?retryWrites=true&w=majority')
 .then(() => console.log("DB ok"))
@@ -10,24 +13,16 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello Dima!');
-});
 
-app.post('/auth/login', (req, res) => {
-  console.log(req.body);
-
-  const token = jwt.sign({
-    email: req.body.email,
-    fullName: "Ваня Иванов"
-  },
-  'secret123'
-);
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
 
   res.json({
     success: true,
-    token,
-  });
+  })
 })
 
 app.listen(4444, (err) => {
