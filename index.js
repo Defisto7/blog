a597f5bb63e4585450acc7beb2533ecb7ad3a1b3
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-07-24 12:18:06 +0300

    Master
    add cors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import multer from 'multer';
 import mongoose from 'mongoose';
-import { registerValidation, loginValidation, postCreateValidation } from './validations.js'
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
+import cors from 'cors';
 
 import {checkAuth, handleValidationErrors} from './utils/index.js'
 import { UserController, PostController } from './controllers/index.js'
@@ -25,6 +26,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
 app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
